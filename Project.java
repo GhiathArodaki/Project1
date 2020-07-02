5e33f006d786eb18d1dfcca1cfacecb70840d70e
GhiathArodaki <66985592+GhiathArodaki@users.noreply.github.com>
2020-07-02 01:10:02 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -6,15 +6,15 @@ public class Project {
     
     Scanner user_input = new Scanner(System.in);
     
-    System.out.println("Project\n\n");
+    System.out.println("Welcome!\n\n");
 
-    System.out.println("Enter Something:");
+    System.out.println("What Should I call you?");
     
     String something = user_input.nextLine();
 
     System.out.println("\n\n\n\n");
 
-    System.out.println("You Entered:" + something);
-    System.out.println("I hope that's what you wanted!");
+    System.out.println("Hello," + something);
+    System.out.println("I hope you are doing well today!");
   }
 }

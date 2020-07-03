fe8441eea13f31259b5dad0376308fa18ee373b9
GhiathArodaki <66985592+GhiathArodaki@users.noreply.github.com>
2020-07-03 00:29:31 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -6,15 +6,24 @@ public class Project {
     
     Scanner user_input = new Scanner(System.in);
     
-    System.out.println("Welcome!\n\n");
+    System.out.println("Welcome to the 5th Dimension!\n");
 
-    System.out.println("What Should I call you?");
+    System.out.println("What is your name?");
     
     String something = user_input.nextLine();
-
-    System.out.println("\n\n\n\n");
-
+    
     System.out.println("Hello," + something);
-    System.out.println("I hope you are doing well today!");
+    System.out.println("I hope you are doing well today!\n\n");
+    System.out.println("Let us play a game, guess the first letter of my name!");
+    
+    Scanner user_input = new Scanner(System.in);
+    
+    char myguess = user_input.nextLine();
+    
+    if (char myguess != 'N') {
+      System.out.println("Sorry, you guessed wrong!, try again.");
+      return Scanner; 
+    }
+    
   }
 }

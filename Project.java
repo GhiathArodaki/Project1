cdbd76372131a5bd8630ef301ace499a1bd97114
GhiathArodaki <66985592+GhiathArodaki@users.noreply.github.com>
2020-07-06 11:47:31 -0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -7,6 +7,15 @@ public class Project {
         System.out.println("Hello! my name is Nardin, what is yours?");
         
         String name = user_input.nextLine();
+        System.out.println("Hello!" + name);
+       
+        Scanner user_age = new Scanner(System.in);
+        System.out.println("How old are you?" + name);
+        
+        int age = user_age.nextLine();
+        
+        System.out.println("Amazing! I pray you live a long life!");
+        
         
             
 }
